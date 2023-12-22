79c82b1936f6cf2002ce70e90aaa478a870fe516
Archi <94532438+Champloot@users.noreply.github.com>
2023-12-22 23:53:56 +0300

    Update Binary_search.py

--- Searching/Binary_search.py
+++ Searching/Binary_search.py
@@ -1,1 +1,17 @@
-dfdf
+def binary_search(arr, item):
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        mid = (high+low)//2
+        if int(arr[mid]) == item:
+ 	    return mid
+        elif int(arr[mid]) > item:
+	    high = mid-1
+        elif int(arr[mid]) < item:
+	    low = mid+1
+    return None
+
+arr = [i for i in range(100)]
+
+item = int(input())
+print (binary_search(arr, item))
