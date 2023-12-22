65d50bd614e85a9369804d341fe7d9da8066f2ad
Archi <94532438+Champloot@users.noreply.github.com>
2023-12-23 00:03:01 +0300

    Create Shaker_sort.py

---
+++ Sorting/Shaker_sort.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+def shaker_sort(arr):
+    length = len(arr)
+    start = 0
+    end = length-1
+    need_swap = True
+
+    while need_swap:
+        need_swap = True
+        for i in range(start, end):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                need_swap = True
+        end -= 1
+
+        if not(need_swap):
+            break
+
+        need_swap = False
+        for i in range(end, start, -1):
+            if arr[i] < arr[i-1]:
+                arr[i], arr[i-1] = arr[i-1], arr[i]
+                need_swap = True
+        start += 1
+
+    return arr
+
+
+arr = [randint(-30, 30) for _ in range(20)]
+
+print(arr)
+print(shaker_sort(arr))
