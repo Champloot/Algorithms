31a13a0abb83464321f84b1adc12e4877b40baea
Archi <94532438+Champloot@users.noreply.github.com>
2023-12-23 00:02:44 +0300

    Create Selection_sort.py

---
+++ Sorting/Selection_sort.py
@@ -0,0 +1,27 @@
+from random import randint
+
+
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+	    smallest = arr[i]
+	    smallest_index=i
+		
+    return smallest_index
+
+
+def selection_sort(arr):
+    sortArr = []
+    for i in range(len(arr)):
+	smallest = find_smallest(arr)
+        sortArr.append(arr.pop(smallest))
+	
+    return sortArr
+
+
+arr = [randint(-30, 30) for _ in range(20)]
+
+print(arr)
+print(selection_sort(arr))
