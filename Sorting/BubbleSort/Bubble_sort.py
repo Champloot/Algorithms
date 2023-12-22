bb0f4a5ede567a786fa83db98fd5b923302cca67
Archi <94532438+Champloot@users.noreply.github.com>
2023-12-22 13:31:27 +0300

    Update Bubble_sort.py

--- Sorting/BubbleSort/Bubble_sort.py
+++ Sorting/BubbleSort/Bubble_sort.py
@@ -1,1 +1,15 @@
-yy
+from random import randint
+
+
+def bubble_sort(arr):
+    for j in range(len(arr)-1):
+        for i in range(len(arr)-j-1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+    return arr
+
+
+arr = [randint(-30, 30) for _ in range(20)]
+
+print(arr)
+print(bubble_sort(arr))
